db518cdd5d517f8619039bb51a954b005cff7a85
Rodrigo Carneiro <rodrigo_carneiro@ymail.com>
2018-09-13 18:31:31 +0100

    (WIP) Enable CORS on Nginx Working on convert methods to restful resources

--- api/run.py
+++ api/run.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python
-from project import app
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_cors import CORS
 
-app.run(host='0.0.0.0')
+app = Flask(__name__)
+#cors = CORS(app, resources={r"*": {"origins": "*"}})
+api = Api(app)
+
+class Ping(Resource):
+    def get(self):
+        return {'message': 'pong'}
+
+api.add_resource(Ping, '/ping')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
--- frontend/src/components/Books.vue
+++ frontend/src/components/Books.vue
@@ -144,7 +144,7 @@ export default {
   },
   methods: {
     getBooks() {
-      const path = 'http://localhost:5000/books';
+      const path = 'http://api:8000/books';
       axios.get(path)
         .then((res) => {
           this.books = res.data.books;
@@ -155,7 +155,7 @@ export default {
         });
     },
     addBook(payload) {
-      const path = 'http://localhost:5000/books';
+      const path = 'http://api:8000/books';
       axios.post(path, payload)
         .then(() => {
           this.getBooks();
@@ -211,7 +211,7 @@ export default {
       this.updateBook(payload, this.editForm.id);
     },
     updateBook(payload, bookID) {
-      const path = `http://localhost:5000/books/${bookID}`;
+      const path = `http://api:8000/books/${bookID}`;
       axios.put(path, payload)
         .then(() => {
           this.getBooks();
@@ -231,7 +231,7 @@ export default {
       this.getBooks(); // why?
     },
     removeBook(bookID) {
-      const path = `http://localhost:5000/books/${bookID}`;
+      const path = `http://api:8000/books/${bookID}`;
       axios.delete(path)
         .then(() => {
           this.getBooks();
