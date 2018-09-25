c247bf311cda9f6074897f273f06dccb3d195f0e
Rodrigo Carneiro <rodrigo_carneiro@ymail.com>
2018-09-25 19:36:03 +0100

    Make blueprint namespace work

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Blueprint
 from flask_restplus import Api, Resource
-from .books import api as books
+from .books import api as books_api
 
-blueprint = Blueprint('api', __name__, url_prefix='/api/1')
+blueprint = Blueprint('api', __name__, url_prefix='/api')
 api = Api(blueprint,
-    title='My Title',
+    title='Books Api',
     version='1.0',
-    description='A description',
+    description='A simple book Api',
     # All API metadatas
 )
 
-api.add_namespace(books)
+api.add_namespace(books_api)
--- frontend/config/index.js
+++ frontend/config/index.js
@@ -6,7 +6,7 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
+    rootApi: '"http://localhost:5000/api"',
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
