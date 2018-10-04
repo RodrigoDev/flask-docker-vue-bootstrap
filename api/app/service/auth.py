d204721c357483eaf4fb01c73d4ee0a41c0c0acc
Rodrigo Carneiro <rodrigo_carneiro@ymail.com>
2018-10-04 18:03:21 +0100

    (WIP) Auth and Mongo ORM

---
+++ api/app/service/auth.py
@@ -0,0 +1,15 @@
+from ..model.user import User
+
+class Auth:
+
+    @staticmethod
+    def login(data):
+        pass
+
+    @staticmethod
+    def logout(data):
+        pass
+
+    @staticmethod
+    def get_logged_in_user(request):
+        pass
--- api/config.py
+++ api/config.py
@@ -1,15 +1,19 @@
-from pymongo import MongoClient
-import os, pprint
+from mongoengine import connect
+import os
+import pprint
 
 
 env = os.environ.get('FLASK_ENV', 'local')
-mongo_uri = "mongodb://{}:27017".format("mongodb" if env == "dev" else "localhost")
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'put_your_secret_key_here'
 
+APP_NAME = 'tada_app'
 DB_NAME = 'tada'
 
-DATABASE = MongoClient(mongo_uri)[DB_NAME]
+mongo_uri = "mongodb://{}:27017/{}".format("mongodb" if env == "dev" else "localhost",
+                                           DB_NAME)
+
+DATABASE = connect(APP_NAME, mongo_uri)
 
 DEBUG = True
